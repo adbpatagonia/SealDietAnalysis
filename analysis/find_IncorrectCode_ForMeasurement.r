930ccaa84e1a67abfc0292a1bc175fe5c6730ca2
A Buren <aburen@mun.ca>
2019-07-18 06:27:14 -0700

    incorrect code for measurements

---
+++ analysis/find_IncorrectCode_ForMeasurement.r
@@ -0,0 +1,7 @@
+source('analysis/01_calculate_length_weight.R')
+library(tidyverse)
+
+diet %>%
+  filter(codemeasuredestimated == 1 & is.na(pMVmm) & !is.na(length)) %>%
+  select(idsex, prey, preycode, codemeasuredestimated, pMVmm, length, weight, idpreyitem) %>%
+  write.csv(file = 'output/IncorrectCodeForMeasurement_july2019.csv', row.names = FALSE)
